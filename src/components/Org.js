cff64c1e20b7e206a3d18921d47de9b657ba87c7
Rahul Gorai <rahul.gorai.3141@gmail.com>
2021-05-29 20:07:09 +0530

    Make organisation description dynamic

--- src/components/Org.js
+++ src/components/Org.js
@@ -3,21 +3,21 @@ import { Card, CardGroup, CardDeck, Button } from "react-bootstrap";
 
 function Org() {
   let organisations = [
-    "Organisation 1",
-    "Organisation 2",
-    "Organisation 3",
-    "Organisation 4",
-    "Organisation 5",
-    "Organisation 6",
-    "Organisation 7",
-    "Organisation 8",
-    "Organisation 9",
+    { name: "Organisation 1", description: "Some quick example text to build on the card title and make up the bulk of the card's content." },
+    { name: "Organisation 2", description: "Some quick example text to build on the card title and make up the bulk of the card's content." },
+    { name: "Organisation 3", description: "Some quick example text to build on the card title and make up the bulk of the card's content." },
+    { name: "Organisation 4", description: "Some quick example text to build on the card title and make up the bulk of the card's content." },
+    { name: "Organisation 5", description: "Some quick example text to build on the card title and make up the bulk of the card's content." },
+    { name: "Organisation 6", description: "Some quick example text to build on the card title and make up the bulk of the card's content." },
+    { name: "Organisation 7", description: "Some quick example text to build on the card title and make up the bulk of the card's content." },
+    { name: "Organisation 8", description: "Some quick example text to build on the card title and make up the bulk of the card's content." },
+    { name: "Organisation 9", description: "Some quick example text to build on the card title and make up the bulk of the card's content." }
   ];
   return (
     <div
       style={{ textAlign: "center", marginLeft: "25px", marginRight: "25px" }}
     >
-      {organisations.map((variant, idx) => (
+      {organisations.map((org, idx) => (
         <Card
           bg="dark"
           key={idx}
@@ -25,12 +25,11 @@ function Org() {
           style={{ width: "18rem", display: "inline-block", margin: "20px" }}
           className="mb-2"
         >
-          <Card.Header bg="warning">{variant}</Card.Header>
+          <Card.Header bg="warning">{org.name}</Card.Header>
           <Card.Body>
-            <Card.Title>{variant}</Card.Title>
+            <Card.Title>{org.name}</Card.Title>
             <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+              {org.description}
             </Card.Text>
           </Card.Body>
           <Card.Footer>
