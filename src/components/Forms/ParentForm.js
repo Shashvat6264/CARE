7d534932dd035c2e1007b5e90a66de60cf3d30a4
Shashvat Gupta <shashvat6264@gmail.com>
2021-05-29 20:58:06 +0530

    Form created attributes remain

--- src/components/Forms/ParentForm.js
+++ src/components/Forms/ParentForm.js
@@ -4,10 +4,9 @@ function ParentForm() {
     return (
         <div>
             <h1>Sign up</h1>
-            <input type="text" name="username" placeholder="Username" />
+            <input type="text" name="name" placeholder="Name of Guardian" />
             <input type="text" name="email" placeholder="E-mail" />
-            <input type="password" name="password" placeholder="Password" />
-            <input type="password" name="password2" placeholder="Retype password" />
+            <input type="text" name="address" placeholder="Address" />
             <input type="submit" name="signup_submit" value="Sign me up" />           
         </div>
     )
--- src/components/Forms/ReporterForm.js
+++ src/components/Forms/ReporterForm.js
@@ -6,8 +6,7 @@ function ReporterForm() {
             <h1>Sign up</h1>
             <input type="text" name="username" placeholder="Username" />
             <input type="text" name="email" placeholder="E-mail" />
-            <input type="password" name="password" placeholder="Password" />
-            <input type="password" name="password2" placeholder="Retype password" />
+            <input type="text" name="address" placeholder="Address" />
             <input type="submit" name="signup_submit" value="Sign me up" />           
         </div>
     )
