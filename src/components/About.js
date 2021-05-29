53e093e3487da41f2bf2f2ea56c09ae5dee6d909
Shashvat Gupta <shashvat6264@gmail.com>
2021-05-29 16:16:41 +0530

    Added Bootstrap

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import {Button} from 'react-bootstrap'
 
 function About() {
     return (
         <div>
             <h1>About page</h1>
+            <Button>Hi</Button>
         </div>
     )
 }
